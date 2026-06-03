649781785397cf8922fe6bea6c03c4cd0f5c947d
ajeebgeorge <ajeebgeorge@gmail.com>
2026-06-03 07:47:10 -0400

    feature: openAI calls

--- openAIfunction.py
+++ openAIfunction.py
@@ -2,15 +2,21 @@ from openai import OpenAI
 import os
 
 api_key= os.environ.get('OpenAIKey')
-print(api_key)
+
+messages = []
 client = OpenAI(api_key=api_key)
+MODEL = 'gpt-4o-mini'
+system_prompt = {"role":"system", "content":"You are a data engineering expert."}
+user_prompt1 = {"role":"user", "content":"What is a data pipeline?"}
+messages.append(system_prompt)
+messages.append(user_prompt1)
+print(messages)
 
-response = client.chat.completions.create(
-    model = 'gpt-4o-mini',
-    messages =[
-        {"role":"system", "content":"You are a data engineering expert."},
-        {"role":"user", "content":"What causes a pipeline to fail silently?"}
-    ]
-)
+response1 = client.chat.completions.create(model = MODEL,messages = messages)
+messages.append({"role":"assistant","content":response1.choices[0].message.content})
+user_prompt2 = {"role":"user", "content":"What causes it to fail?"}
+messages.append(user_prompt2)
+print(messages)
 
-print(response)
+response2 = client.chat.completions.create(model = MODEL,messages = messages)
+print(response2.choices[0].message.content)
